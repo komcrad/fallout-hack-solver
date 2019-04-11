07fc1a90fa90b35b30e4e01e1dd5ee0604a48a97
komcrad <konradt95@gmail.com>
2019-04-11 17:32:42 -0500

    Stage for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fallout-hack-solver "0.1.0-SNAPSHOT"
+(defproject fallout-hack-solver "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
